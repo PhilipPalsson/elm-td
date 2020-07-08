8f5c3ab68666306b119d45f82742ecbb47e2159d
Philip Pålsson <philip.palsson@insurello.se>
2020-07-09 00:06:08 +0200

    Updates

---
+++ precache-manifest.29ee16f660a5ce4125a79dd5f4372bcd.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3fc3e8ec3f4615853005eedcffba2f73",
+    "url": "/elm-td/.DS_Store"
+  },
+  {
+    "revision": "987da976a8b2a4169346a497ba84749d",
+    "url": "/elm-td/favicon.ico"
+  },
+  {
+    "revision": "194577a7e20bdcc7afbb718f502c134c",
+    "url": "/elm-td/images/.DS_Store"
+  },
+  {
+    "revision": "d6a84d248fcd8f58514c9c5c0fd45b9d",
+    "url": "/elm-td/images/base_tower.png"
+  },
+  {
+    "revision": "671fd0f4ef2c569b4ab48eba91947993",
+    "url": "/elm-td/images/bat.png"
+  },
+  {
+    "revision": "19d241594cd115838fd548c127225b3c",
+    "url": "/elm-td/images/cave.png"
+  },
+  {
+    "revision": "71d5c91a58047f099c06edc00d87f74c",
+    "url": "/elm-td/images/fort.png"
+  },
+  {
+    "revision": "2700094a0f56ecf71694b80b14fde39d",
+    "url": "/elm-td/images/ghost.png"
+  },
+  {
+    "revision": "c89fbfa0bab31bc9b5cc350ef7c713de",
+    "url": "/elm-td/images/stone.png"
+  },
+  {
+    "revision": "15c9288041f3629eb4840fb6391d492d",
+    "url": "/elm-td/index.html"
+  },
+  {
+    "revision": "6ead519905b7b907c00965be37dd5cd7",
+    "url": "/elm-td/logo.svg"
+  },
+  {
+    "revision": "1c82903c2994c2507b385b9042bbfac5",
+    "url": "/elm-td/manifest.json"
+  },
+  {
+    "revision": "7aa4050ed335a9513c47",
+    "url": "/elm-td/static/css/main.48b01631.chunk.css"
+  },
+  {
+    "revision": "7aa4050ed335a9513c47",
+    "url": "/elm-td/static/js/main.7aa4050e.chunk.js"
+  },
+  {
+    "revision": "ba8e4d3bf56958cc72f1",
+    "url": "/elm-td/static/js/runtime~main.ba8e4d3b.js"
+  },
+  {
+    "revision": "b9063112a1dd4b93007d",
+    "url": "/elm-td/static/js/vendors~main.b9063112.chunk.js"
+  }
+]);
